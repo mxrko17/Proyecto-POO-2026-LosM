1ce8087c941bb77f2ba4c3b32775a9f37124010a
mxrko17 <marco.basualto2501@alumnos.ubiobio.cl>
2026-04-07 16:27:02 -0400

    primer commit

---
+++ src/Cliente.java
@@ -0,0 +1,17 @@
+public class Cliente {
+    private String email;
+
+    public Cliente (idPersona id, Nombre nom, String email) {
+        this.idPersona = id;
+        this.Nombre = nom;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/Pasajero.java
@@ -0,0 +1,20 @@
+public class Pasajero {
+    private Nombre nomContacto;
+    private String fonoContacto;
+
+    public Nombre getNomContacto() {
+        return nomContacto;
+    }
+
+    public String getFonoContacto() {
+        return fonoContacto;
+    }
+
+    public void setNomContacto(Nombre nomContacto) {
+        this.nomContacto = nomContacto;
+    }
+
+    public void setFonoContacto(String fonoContacto) {
+        this.fonoContacto = fonoContacto;
+    }
+}
