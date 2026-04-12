d172687f141df49195df9bfdd5ec2351444269e4
sr7890-estudio <matias.cruz2501@alumnos.ubiobio.cl>
2026-04-12 19:39:00 -0400

    Agrego-Persona

---
+++ src/Persona.java
@@ -0,0 +1,38 @@
+class Persona {
+    private idPersona idPersona;
+    private Nombre nombreCompleto;
+    private String telefono;
+
+    public Persona(idPersona idPersona, Nombre nombreCompleto) {
+        this.idPersona = idPersona;
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public idPersona getIdPersona() {
+        return idPersona;
+    }
+
+    public Nombre getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setNombreCompleto(Nombre nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String toString(){
+        return "Pasajero: " + nombreCompleto + " id: " + idPersona + " telefono: " + telefono;
+    }
+
+    public boolean equals(Object otro) {
+
+    }
+}
