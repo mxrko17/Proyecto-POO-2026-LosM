91ca3ccb5d1d6e56b036607623e5290346e9bcc4
mxrko17 <marco.basualto2501@alumnos.ubiobio.cl>
2026-05-15 20:32:16 -0400

    Implementacion de las clases Direccion y RunTimeException

---
+++ src/RunTimeException.java
@@ -0,0 +1,9 @@
+public class RunTimeException extends RuntimeException {
+    public RunTimeException(String msg) {
+        super(msg);
+    }
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
