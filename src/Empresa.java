95bf6cf4d744965451611d658abc3e2144fd004e
sr7890-estudio <matias.cruz2501@alumnos.ubiobio.cl>
2026-05-20 18:05:09 -0400

    Finalizacion de la clase empresa

--- src/Empresa.java
+++ src/Empresa.java
@@ -1,208 +1,69 @@
-public class Empresa {
+import java.util.ArrayList;
 
+public class Empresa {
     private Rut rut;
     private String nombre;
     private String url;
-
-    private Bus[] buses;
-    private Conductor[] conductores;
-    private Auxiliar[] auxiliares;
+    private ArrayList<Bus> buses;
+    private ArrayList<Tripulante> tripulantes;
 
     public Empresa(Rut rut, String nombre) {
         this.rut = rut;
         this.nombre = nombre;
-
-        buses = new Bus[100];
-        conductores = new Conductor[100];
-        auxiliares = new Auxiliar[100];
+        this.buses = new ArrayList<>();
+        this.tripulantes = new ArrayList<>();
     }
 
-    public Rut getRut() {
-        return rut;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
+    public Rut getRut() { return rut; }
+    public String getNombre() { return nombre; }
+    public String getUrl() { return url; }
+    public void setUrl(String url) { this.url = url; }
 
     public void addBus(Bus bus) {
-
-        for (int i = 0; i < buses.length; i++) {
-
-            if (buses[i] == null) {
-                buses[i] = bus;
-                break;
-            }
-        }
+        if (!buses.contains(bus)) buses.add(bus);
     }
 
     public Bus[] getBuses() {
-        return buses;
+        return buses.toArray(new Bus[0]);
     }
 
-    public boolean addConductor(IdPersona id, Nombre nom, Direccion dir) {
-
-        for (int i = 0; i < conductores.length; i++) {
-
-            if (conductores[i] != null &&
-                    conductores[i].getIdPersona().equals(id)) {
-
-                return false;
-            }
+    // Retorna verdadero si puede realizarse la acción, falso en caso contrario
+    public boolean addConductor(idPersona id, Nombre nom, Direccion dir) {
+        for (Tripulante t : tripulantes) {
+            if (t.getIdPersona().equals(id)) return false;
         }
-
-        for (int i = 0; i < auxiliares.length; i++) {
-
-            if (auxiliares[i] != null &&
-                    auxiliares[i].getIdPersona().equals(id)) {
-
-                return false;
-            }
-        }
-
-        for (int i = 0; i < conductores.length; i++) {
-
-            if (conductores[i] == null) {
-
-                conductores[i] = new Conductor(id, nom, dir);
-                return true;
-            }
-        }
-
-        return false;
+        Conductor c = new Conductor(id, nom, dir);
+        tripulantes.add(c);
+        return true;
     }
 
-    public boolean addAuxiliar(IdPersona id, Nombre nom, Direccion dir) {
-
-        for (int i = 0; i < auxiliares.length; i++) {
-
-            if (auxiliares[i] != null &&
-                    auxiliares[i].getIdPersona().equals(id)) {
-
-                return false;
-            }
+    // Retorna verdadero si puede realizarse, falso si no puede hacer la accion
+    public boolean addAuxiliar(idPersona id, Nombre nom, Direccion dir) {
+        for (Tripulante t : tripulantes) {
+            if (t.getIdPersona().equals(id)) return false;
         }
-
-        for (int i = 0; i < conductores.length; i++) {
-
-            if (conductores[i] != null &&
-                    conductores[i].getIdPersona().equals(id)) {
-
-                return false;
-            }
-        }
-
-        for (int i = 0; i < auxiliares.length; i++) {
-
-            if (auxiliares[i] == null) {
-
-                auxiliares[i] = new Auxiliar(id, nom, dir);
-                return true;
-            }
-        }
-
-        return false;
+        Auxiliar a = new Auxiliar(id, nom, dir);
+        tripulantes.add(a);
+        return true;
     }
 
+    // Retorna un arreglo unidimensional con todos los tripulantes
     public Tripulante[] getTripulantes() {
-
-        int total = 0;
-
-        for (int i = 0; i < conductores.length; i++) {
-
-            if (conductores[i] != null) {
-                total++;
-            }
-        }
-
-        for (int i = 0; i < auxiliares.length; i++) {
-
-            if (auxiliares[i] != null) {
-                total++;
-            }
-        }
-
-        Tripulante[] tripulantes = new Tripulante[total];
-
-        int pos = 0;
-
-        for (int i = 0; i < conductores.length; i++) {
-
-            if (conductores[i] != null) {
-
-                tripulantes[pos] = conductores[i];
-                pos++;
-            }
-        }
-
-        for (int i = 0; i < auxiliares.length; i++) {
-
-            if (auxiliares[i] != null) {
-
-                tripulantes[pos] = auxiliares[i];
-                pos++;
-            }
-        }
-
-        return tripulantes;
+        return tripulantes.toArray(new Tripulante[0]);
     }
 
+    // Retorna un arreglo unidimensional con todas las ventas asociadas a la empresa
     public Venta[] getVentas() {
-
-        Venta[] ventas = new Venta[1000];
-        int pos = 0;
-
-        for (int i = 0; i < buses.length; i++) {
-
-            if (buses[i] != null) {
-
-                Viaje[] viajes = buses[i].getViajes();
-
-                for (int j = 0; j < viajes.length; j++) {
-
-                    if (viajes[j] != null) {
-
-                        Venta[] ventasViaje = viajes[j].getVentas();
-
-                        for (int k = 0; k < ventasViaje.length; k++) {
-
-                            if (ventasViaje[k] != null) {
-
-                                boolean repetida = false;
-
-                                for (int x = 0; x < pos; x++) {
-
-                                    if (ventas[x].equals(ventasViaje[k])) {
-                                        repetida = true;
-                                    }
-                                }
-
-                                if (!repetida) {
-
-                                    ventas[pos] = ventasViaje[k];
-                                    pos++;
-                                }
-                            }
-                        }
+        ArrayList<Venta> ventasEmpresa = new ArrayList<>();
+        for (Bus b : buses) {
+            for (Viaje v : b.getViajes()) {
+                for (Venta venta : v.getVentas()) {
+                    if (!ventasEmpresa.contains(venta)) {
+                        ventasEmpresa.add(venta);
                     }
                 }
             }
         }
-
-        Venta[] resultado = new Venta[pos];
-
-        for (int i = 0; i < pos; i++) {
-            resultado[i] = ventas[i];
-        }
-
-        return resultado;
+        return ventasEmpresa.toArray(new Venta[0]);
     }
 }
