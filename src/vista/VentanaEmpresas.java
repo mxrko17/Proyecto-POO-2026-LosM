6c51617d2f3d89c27902eb3bb09140bff67f04b4
mxrko17 <marco.basualto2501@alumnos.ubiobio.cl>
2026-07-12 00:23:07 -0400

    Implementacion de ventanas

---
+++ src/vista/VentanaEmpresas.java
@@ -0,0 +1,9 @@
+package vista;
+
+import javax.swing.*;
+
+public class VentanaEmpresas {
+    private JPanel ventanaEmpresas;
+    private JTable tableEmpresa;
+    private JButton actualizarListaButton;
+}
---
+++ src/vista/VentanaVentasEmpresa.java
@@ -0,0 +1,10 @@
+package vista;
+
+import javax.swing.*;
+
+public class VentanaVentasEmpresa {
+    private JPanel ventasEmpresa;
+    private JTextField textFieldRutEmpresa;
+    private JButton buscarButton;
+    private JTable tableVentasEmpresas;
+}
