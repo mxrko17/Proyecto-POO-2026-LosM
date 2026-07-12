ff521e348ae4c0e6e960f0e506dad4d98361fc3b
sr7890-estudio <matias.cruz2501@alumnos.ubiobio.cl>
2026-07-12 00:22:10 -0400

    Creacion de ventanas: -VentanaPrincipal -VentanaVentaPasajes

---
+++ src/VentanaPrincipal.java
@@ -0,0 +1,14 @@
+package vista;
+
+import javax.swing.*;
+
+public class VentanaPrincipal {
+    private JButton leerGuardarRecuperarButton;
+    private JButton ventaDePasajeButton;
+    private JButton crearUnViajeButton;
+    private JButton empresasButton;
+    private JButton terminalesButton;
+    private JButton ventasButton;
+    private JButton salirButton;
+    private JPanel menuDeOpciones;
+}
---
+++ src/VentanaVentaPasajes.java
@@ -0,0 +1,15 @@
+package vista;
+
+import javax.swing.*;
+
+public class VentanaVentaPasajes {
+    private JButton generarBoletaButton;
+    private JTextField textFieldRut;
+    private JTextField textFieldFechaViaje;
+    private JComboBox comboBoxComunaOrigen;
+    private JComboBox comboBoxComunaDestino;
+    private JButton viajeButton;
+    private JButton ventaButton;
+    private JTextField textFieldPasajero;
+    private JPanel ventaPasajes;
+}
